a0fcab757d2659fa8ccc74037ad9f5ca410c7ec0
sadid369 <sadid.jones@gmail.com>
2023-12-19 00:23:09 +0600

    end explicit animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animation_starter_code/explicit_examples/list_animation.dart';
+import 'package:animation_starter_code/explicit_examples/loading_animation.dart';
 import 'package:animation_starter_code/explicit_examples/login_animation.dart';
 import 'package:animation_starter_code/implicit_examples/animated_color_pallete.dart';
 import 'package:animation_starter_code/implicit_examples/animated_shopping_cart_button.dart';
@@ -9,18 +10,28 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Animation Tutorial Rivaan',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ListAnimation(),
+      home: const RadialProgressAnimation(progress: 0.65, color: Colors.blue),
     );
   }
 }
