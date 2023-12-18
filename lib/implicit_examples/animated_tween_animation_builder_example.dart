3feb4c915daf65842f310b06fd76dda5297d32e3
sadid369 <sadid.jones@gmail.com>
2023-12-18 23:25:03 +0600

    login animation completed

--- lib/implicit_examples/animated_tween_animation_builder_example.dart
+++ lib/implicit_examples/animated_tween_animation_builder_example.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class PulsatingCircleAnimation extends StatelessWidget {
   const PulsatingCircleAnimation({super.key});
-  final double size = 200;
+  // final double size = 200;
 
   @override
   Widget build(BuildContext context) {
@@ -11,20 +11,30 @@ class PulsatingCircleAnimation extends StatelessWidget {
         title: const Text('Pulsating Circle Animation'),
       ),
       body: Center(
-        child: Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue.withOpacity(0.5),
-                blurRadius: size,
-                spreadRadius: size / 2,
+        child: TweenAnimationBuilder(
+          tween: Tween<double>(begin: 0, end: 200),
+          curve: Curves.bounceInOut,
+          duration: Duration(milliseconds: 1500),
+          builder: (context, size, widgets) {
+            return Container(
+              alignment: Alignment.center,
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.5),
+                    blurRadius: size,
+                    spreadRadius: size / 2,
+                  ),
+                ],
               ),
-            ],
-          ),
+              child: widgets,
+            );
+          },
+          child: const Text('Hello world'),
         ),
       ),
     );
