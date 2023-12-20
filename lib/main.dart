68ff8890b1c870a9c1463740d5d1ebfac26a4167
sadid369 <sadid.jones@gmail.com>
2023-12-20 21:00:01 +0600

    page_route_builder_animation completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:animation_starter_code/explicit_examples/login_animation.dart';
 import 'package:animation_starter_code/implicit_examples/animated_color_pallete.dart';
 import 'package:animation_starter_code/implicit_examples/animated_shopping_cart_button.dart';
 import 'package:animation_starter_code/implicit_examples/animated_tween_animation_builder_example.dart';
+import 'package:animation_starter_code/page_route_builder_animation/splash_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,12 +27,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Animation Tutorial Rivaans',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RadialProgressAnimation(progress: 0.65, color: Colors.blue),
+      home: const SplashAnimation(),
     );
   }
 }
--- lib/page_route_builder_animation/splash_animation.dart
+++ lib/page_route_builder_animation/splash_animation.dart
@@ -1,23 +1,75 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashAnimation extends StatelessWidget {
+class SplashAnimation extends StatefulWidget {
   const SplashAnimation({super.key});
+
+  @override
+  State<SplashAnimation> createState() => _SplashAnimationState();
+}
+
+class _SplashAnimationState extends State<SplashAnimation>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    scaleAnimation = Tween<double>(begin: 1, end: 10).animate(controller);
+    controller.addListener(() {
+      if (controller.isCompleted) {
+        Navigator.of(context).push(
+            // PageRouteBuilder(
+            //   transitionDuration: Duration(milliseconds: 500),
+            //   pageBuilder: (context, animation, secondaryAnimation) {
+            //     return const Destination();
+            //   },
+            //   transitionsBuilder:
+            //       (context, animation, secondaryAnimation, child) {
+            //     final Animation<Offset> offSetAnimation =
+            //         Tween<Offset>(begin: Offset(0, 1), end: Offset.zero)
+            //             .animate(animation);
+            //     return SlideTransition(
+            //       position: offSetAnimation,
+            //       child: child,
+            //     );
+            //   },
+            // ),
+            CustomPageRouteBuilder(route: const Destination()));
+        Timer(const Duration(milliseconds: 500), () {
+          controller.reset();
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-            shape: BoxShape.circle,
-          ),
-          child: Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue,
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: InkWell(
+            onTap: () {
+              controller.forward();
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+              ),
             ),
           ),
         ),
@@ -38,4 +90,23 @@ class Destination extends StatelessWidget {
       ),
     );
   }
+}
+
+class CustomPageRouteBuilder extends PageRouteBuilder {
+  final Widget route;
+  CustomPageRouteBuilder({required this.route})
+      : super(
+          pageBuilder: (context, animation, secondaryAnimation) {
+            return route;
+          },
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            final Animation<Offset> offSetAnimation =
+                Tween<Offset>(begin: const Offset(0, 1), end: Offset.zero)
+                    .animate(animation);
+            return SlideTransition(
+              position: offSetAnimation,
+              child: child,
+            );
+          },
+        );
 }
